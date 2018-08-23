aa3aeb990f37842e12856e26d4bf05ee470cb60f
Ralph Field <fieldr92@gmail.com>
2018-08-23 17:31:00 +0100

    ♻️ index.js refactor / addition - loadAssets now called pushAssetsToImgElements - createElements started but more needed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import data from './data/dataTest';
 
 class Banner {
@@ -9,14 +8,22 @@ class Banner {
     this.imgElements = [];
   }
 
-  loadQueue() {    
-    const images = this.data.frames.forEach(frame => {
+  pushAssetsToImgElements() {    
+    this.data.frames.forEach(frame => {
       frame.layers.forEach(layer => {
         if (layer.src) { this.imgElements.push(layer.src) }
       });
     });
-    console.log(images);
-    return images;
+    return this;
+  }
+
+  createElements() {
+    this.data.frames.forEach(frame => {
+      frame.layers.forEach(layer => {
+        
+      });
+    });
+    return this;
     // const queue = new createjs.loadQueue();
     // queue.on("complete", handleComplete, this);
 
@@ -28,10 +35,8 @@ class Banner {
     //   const image = queue.getResult("")
     // }
   }
-
-  createElements() {
-  }
-
 }
 
-new Banner({width: 300, height: 250, data}).loadQueue();
+new Banner({width: 300, height: 250, data})
+  .pushAssetsToImgElements()
+  .createElements();
