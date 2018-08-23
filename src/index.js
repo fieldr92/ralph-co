146ce185f22af4520fb27351c31ef7bcce33acc8
Ralph Field <fieldr92@gmail.com>
2018-08-23 16:03:30 +0100

    ✨ Banner class added to index.js - constructor made - loadQueue method started

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+import path from 'path';
 import data from './data/dataTest';
 
-console.log(data.frames[0].layers[0].src);
+class Banner {
+  constructor({ width, height, data }) {
+    this.width = width;
+    this.height = height;
+    this.data = data;
+    this.imgElements = [];
+  }
+
+  loadQueue() {    
+    const hello = this.data.frames.forEach(frame => {
+      this.imgElements = frame.layers.map(layer => {
+        return layer.src;
+      })
+    });
+    console.log('hello', hello);
+    // const queue = new createjs.loadQueue();
+    // queue.on("complete", handleComplete, this);
+
+    // queue.loadManifest([
+    //   { id: "myImage", src:"path/to/myImage.jpg" },
+    //   { id: "myImage2", src:"path/to/myImage2.jpg" }
+    // ]);
+    // const handleComplete = () => {
+    //   const image = queue.getResult("")
+    // }
+  }
+
+  createElements() {
+
+  }
+
+}
+
+new Banner({width: 300, height: 250, data}).loadQueue();
