a7e0d2cea98bec77256e153857dbfd0742616f78
fieldr <fieldr92@gmail.com>
2018-08-28 21:50:41 +0100

    ♻️ Removed constructor destructuring for simplicity - added animator() and Animations import

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import data from './data/dataTest';
 import LoadElements from './classes/LoadElements';
+import Animations from './classes/Animations';
 
 class Banner {
-  constructor({data}) {
+  constructor(data) {
     this.width = data.width;
     this.height = data.height;
     this.data = data;
     this.frames = [];
-    this.manifestImages = [];
     this.assetPath = "./assets/";
   }
 
-  init() {    
+  init() {
     this.stage = new createjs.Stage(document.getElementById('stage'));
     createjs.Ticker.addEventListener('tick', this.handleTick.bind(this));
-    const elements = new LoadElements(this.stage, this.width, this.height, this.data)
+    new LoadElements(this.stage, this.width, this.height, this.data)
       .createManifestImages()
-      .loadQueue();
+      .loadQueue()
+      .animator();
   }
 
   handleTick(event) {    
     this.stage.update();
   }
 
+  buildAnimations() {
+
+  }
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  new Banner({width: 300, height: 250, data})
+  new Banner(data)
   .init();
 })
