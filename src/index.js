924a7aa31be0ba92d685d1ad5d767749d75b8d5a
Ralph Field <fieldr92@gmail.com>
2018-08-28 10:19:52 +0100

    ♻️ Refactor to createElements - moved this.stage.addChild into forEach -removed console.logs

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ export default class Banner {
 
   init() {    
     this.stage = new createjs.Stage(document.getElementById('stage'));
-    // this.stage.enableMouseOver(10);
     this.pushAssetsToImgElements();
     this.createManifest();
     createjs.Ticker.addEventListener('tick', this.handleTick.bind(this));
@@ -26,7 +25,6 @@ export default class Banner {
 
   pushAssetsToImgElements() {
     this.imgElements.push(this.data.background);
-
     this.data.frames.forEach(frame => {
       frame.layers.forEach(layer => {
         if (layer.src) { this.imgElements.push(layer.src) }
@@ -44,8 +42,6 @@ export default class Banner {
   loadQueue() {
     this.loader = new createjs.LoadQueue(true);
     this.loader.on("complete", this.handleComplete.bind(this));
-    console.log(this.manifestImages);
-    
     this.loader.loadManifest( this.manifestImages );
   }
 
@@ -58,15 +54,21 @@ export default class Banner {
       const obj = {};
       const key = img.id;
       obj[key] = new createjs.Bitmap(this.loader.getResult(img.id));
-      this.elements = Object.assign(obj, this.elements);
+      this.elements = Object.assign(obj, this.elements);      
+      this.stage.addChild(this.elements[img.id])
+      this.elements[img.id].alpha = 0;
     });
 
-    this.stage.addChild(this.elements.image);
-    this.stage.addChild(this.elements.image2);
-    this.stage.addChild(this.elements.image3);
-    this.stage.addChild(this.elements.spritesheet);
-    console.log(this.stage);
-    console.log(this);    
+    this.elements.image.alpha = 0;
+    
+    setTimeout(() => {
+      createjs.Tween.get(this.elements.image).to({alpha: 1}, 1000);
+    }, 500)
+
+    setTimeout(() => {
+      createjs.Tween.get(this.elements.image).to({x: this.width * 2}, 1000);
+    }, 1500)
+
   }
 
   loadFrames() {
@@ -77,5 +79,7 @@ export default class Banner {
 
 }
 
-window.onload = new Banner({width: 300, height: 250, data})
+document.addEventListener('DOMContentLoaded', () => {
+  new Banner({width: 300, height: 250, data})
   .init();
+})
