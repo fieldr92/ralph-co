000e58722522856faaf71e55fab9f5fe9cbaf1d8
Ralph Field <fieldr92@gmail.com>
2018-08-28 17:03:18 +0100

    ✨ Add loadElements class - handles loading of images and elements - working and elements now showing on the stage

---
+++ src/classes/loadElements.js
@@ -0,0 +1,52 @@
+class loadElements {
+  constructor (stage, width, height, data) {
+    this.stage = stage;
+    this.width = width;
+    this.height = height;
+    this.data = data;
+    this.manifestImages = [];
+    this.elements = {};
+    this.assetPath = "./assets/";
+  }
+
+  createManifestImages() {
+    if (this.data.background) {
+      this.manifestImages.push({
+        id: this.data.background.substr(0, this.data.background.lastIndexOf('.')),
+        src: this.assetPath + this.data.background
+      });
+    }
+    this.data.frames.forEach(frame => {
+      frame.layers.forEach(layer => {
+        if (layer.src) {
+          this.manifestImages.push({
+            id: layer.src.substr(0, layer.src.lastIndexOf('.')),
+            src: this.assetPath + layer.src
+          });
+        }
+      });
+    });
+    return this;
+  }
+
+  loadQueue() {
+    this.loader = new createjs.LoadQueue();
+    this.loader.on("complete", this.createElements.bind(this));
+    this.loader.loadManifest( this.manifestImages );
+  }
+
+  createElements() {
+    this.manifestImages.forEach(img => {
+      const obj = {};
+      const key = img.id;
+      obj[key] = new createjs.Bitmap(this.loader.getResult(img.id));
+      this.elements = Object.assign(obj, this.elements);      
+      this.stage.addChild(this.elements[img.id])
+      this.elements[img.id].alpha = 0;
+    });
+    this.elements.image.alpha = 1;
+  }
+
+}
+
+export default loadElements;
--- src/index.js
+++ src/index.js
@@ -1,80 +1,26 @@
 import data from './data/dataTest';
+import loadElements from './classes/loadElements';
 
-const assetPath = "./assets/";
-
-export default class Banner {
-  constructor({ width, height, data }) {
-    this.width = width;
-    this.height = height;
+class Banner {
+  constructor({data}) {
+    this.width = data.width;
+    this.height = data.height;
     this.data = data;
     this.frames = [];
-    this.imgElements = [];
-    this.elements = {};
+    this.manifestImages = [];
+    this.assetPath = "./assets/";
   }
 
   init() {    
     this.stage = new createjs.Stage(document.getElementById('stage'));
-    this.pushAssetsToImgElements();
-    this.createManifest();
     createjs.Ticker.addEventListener('tick', this.handleTick.bind(this));
+    const elements = new loadElements(this.stage, this.width, this.height, this.data);
+    elements.createManifestImages();
+    elements.loadQueue();
   }
 
   handleTick(event) {    
     this.stage.update();
-  }
-
-  pushAssetsToImgElements() {
-    this.imgElements.push(this.data.background);
-    this.data.frames.forEach(frame => {
-      frame.layers.forEach(layer => {
-        if (layer.src) { this.imgElements.push(layer.src) }
-      });
-    });
-  }
-
-  createManifest() {
-    this.manifestImages = this.imgElements.map(el => {
-      return { id: el.substr(0, el.lastIndexOf('.')), src: assetPath + el }
-    })
-    this.loadQueue();
-  }
-
-  loadQueue() {
-    this.loader = new createjs.LoadQueue(true);
-    this.loader.on("complete", this.handleComplete.bind(this));
-    this.loader.loadManifest( this.manifestImages );
-  }
-
-  handleComplete() {
-    this.createElements();
-  }
-
-  createElements() {
-    this.manifestImages.forEach(img => {
-      const obj = {};
-      const key = img.id;
-      obj[key] = new createjs.Bitmap(this.loader.getResult(img.id));
-      this.elements = Object.assign(obj, this.elements);      
-      this.stage.addChild(this.elements[img.id])
-      this.elements[img.id].alpha = 0;
-    });
-
-    this.elements.image.alpha = 0;
-    
-    setTimeout(() => {
-      createjs.Tween.get(this.elements.image).to({alpha: 1}, 1000);
-    }, 500)
-
-    setTimeout(() => {
-      createjs.Tween.get(this.elements.image).to({x: this.width * 2}, 1000);
-    }, 1500)
-
-  }
-
-  loadFrames() {
-    this.data.frames.forEach(frame => {
-      this.frames.push(frame);
-    })
   }
 
 }
