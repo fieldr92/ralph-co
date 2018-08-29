f24b0673db9deb8c5ff89c7b11a1bfc75428ae8b
Ralph Field <fieldr92@gmail.com>
2018-08-29 18:02:55 +0100

    ✨ Add Banner.js file - Banner class moved to tile

---
+++ src/classes/Banner.js
@@ -0,0 +1,25 @@
+import LoadElements from './LoadElements';
+
+export default class Banner {
+  constructor(data) {
+    this.width = data.width;
+    this.height = data.height;
+    this.data = data;
+    this.frames = [];
+    this.assetPath = "./assets/";
+  }
+
+  createStage() {
+    console.log('hello');
+    
+    this.stage = new createjs.Stage(document.getElementById('stage'));
+    createjs.Ticker.addEventListener('tick', this.handleTick.bind(this));
+    new LoadElements(this.stage, this.width, this.height, this.data)
+      .createManifestImages()
+      .loadQueue();
+  }
+
+  handleTick(event) {    
+    this.stage.update();
+  }
+}
