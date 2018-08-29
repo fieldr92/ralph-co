9e6eb0d9ec419bacba039eecbc037cef14a4422a
fieldr <fieldr92@gmail.com>
2018-08-29 09:24:08 +0100

    ✨ Add switch statement to animator method - working only if animation-in === fade

--- src/classes/loadElements.js
+++ src/classes/loadElements.js
@@ -48,18 +48,51 @@ class LoadElements {
     setTimeout(() => {
       let baseTime = 0;
       this.data.frames.forEach(frame => {
-        frame.layers.forEach(layer => {
+        frame.layers.forEach((layer, i) => {
           this.manifestImages.forEach(img => {
             if (layer.src && img.src.substring(img.src.lastIndexOf('/') + 1) === layer.src) {
-              let animationIn = baseTime + layer["animation-in"]["delay"];
-              let animationOut = layer["animation-out"]["delay"];
+
+              let delayIn = baseTime + layer['animation-in']['delay'];
+              let animationIn = layer['animation-in']['duration'];
+
+              // let delayOut = delayIn + animationIn + layer['animation-out']['delay'];
+              let delayOut = layer['animation-out']['delay'];
+              let animationOut = layer['animation-out']['duration'];
+
+              console.log(`${i} in`, delayIn);
+              console.log(`${i} out`, delayOut);
+
               this.stage.addChild(this.elements[img.id]);
               this.elements[img.id].alpha = 0;
-              new createjs.Tween(this.elements[img.id])
-                .wait(animationIn)
-                .to({alpha: 1}, layer["animation-in"]["duration"])
-                .wait(animationOut)
-                .to({alpha: 0}, layer["animation-out"]["duration"]);
+
+              switch (layer["animation-in"]["type"]) {
+                case "fade":
+                  new createjs.Tween(this.elements[img.id])
+                    .wait(delayIn)
+                    .to({alpha: 1}, animationIn);
+                  break;
+                
+                default: console.log(`${layer['animation-in']['type']} is not an animation type`);
+                  break;
+              }
+
+              switch (layer["animation-out"]["type"]) {
+                case "fade":
+                  new createjs.Tween(this.elements[img.id])
+                    .wait(delayOut)
+                    .to({alpha: 0}, animationOut);
+                  break;
+                
+                default: console.log(`${layer['animation-in']['type']} is not an animation type`);
+                  break;
+              }
+
+
+              // new createjs.Tween(this.elements[img.id])
+              //   .wait(delayIn)
+              //   .to({alpha: 1}, layer["animation-in"]["duration"])
+              //   .wait(delayOut)
+              //   .to({alpha: 0}, layer["animation-out"]["duration"]);
             }
           })
         })
