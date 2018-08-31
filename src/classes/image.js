d2064f4c7ed1d2883d02ce8c8832e02a27c55123
Ralph Field <fieldr92@gmail.com>
2018-08-31 17:08:55 +0100

    🔥 Remove 'type' parameter from constructor

--- src/classes/image.js
+++ src/classes/image.js
@@ -1,8 +1,8 @@
 export default class Image {
-  constructor(id, path, type, top = 0, left = 0) {
+  constructor(id, path, top = 0, left = 0) {
     this.id = id;
     this.path = path;
-    this.element = document.createElement(type);
+    this.element = document.createElement('img');
     this.element.setAttribute('id', this.id);
     this.element.setAttribute('src', path);
     this.element.setAttribute('style', `top: ${top}; left ${left}; opacity: 0`);
