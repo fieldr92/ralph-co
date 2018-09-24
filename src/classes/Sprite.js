7e323333df403ef96b46ead55dfbbb677bc78080
fieldr <fieldr92@gmail.com>
2018-09-24 17:18:03 +0100

    🔥 Remove old conditional code - commented out before, now removed

--- src/classes/Sprite.js
+++ src/classes/Sprite.js
@@ -23,19 +23,7 @@ export default class Sprite extends Animation {
       spriteTimeline
         .to(this.elementId, spriteInDuration, this.layer["animation-in"]["style"], spriteInDelay)
         .to(this.elementId, spriteSpeed, { x: width - this.layer["spriteWidth"], ease:SteppedEase.config((this.layer["spriteWidth"] / width) - 1) })
-        .set(this.elementId, { top: topPosition, x: 0 });
-
-      // if (i === 1) {
-      //   spriteTimeline
-      //     .to(this.elementId, spriteInDuration, this.layer["animation-in"]["style"], spriteInDelay)
-      //     .to(this.elementId, spriteSpeed, { x: width - this.layer["spriteWidth"], ease:SteppedEase.config((this.layer["spriteWidth"] / width) - 1) })
-      //     .set(this.elementId, { top: topPosition, x: 0 });
-      // } else {
-      //   spriteTimeline
-      //     .to(this.elementId, spriteSpeed, { x: width - this.layer["spriteWidth"], ease:SteppedEase.config((this.layer["spriteWidth"] / width) - 1) })
-      //     .set(this.elementId, { top: topPosition, x: 0 });
-      // }
-      
+        .set(this.elementId, { top: topPosition, x: 0 });      
     }
 
     spriteTimeline
