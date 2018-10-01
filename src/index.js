ce34e73d6698dad3e97004730e1772381abd9717
fieldr <fieldr92@gmail.com>
2018-10-01 15:31:12 +0100

    🎨 Change to order of code in index.js

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import Banner from './classes/Banner';
 
 const jsonFile = `data.json`;
 
-const loadJSON = (file) => {
-  return fetch(file)
-    .then(res => res.json())
-    .catch(err => console.log(err));
+const init = () => {
+  loadJSON(jsonFile).then(data => {
+    new Banner(data)
+      .createBackground()
+      .createElements()
+      .animateFrames()
+      .start();
+    }
+  );
 }
 
-const init = () => {
-  loadJSON(jsonFile)
-    .then(data => {
-      new Banner(data)
-        .createBackground()
-        .createElements()
-        .animateFrames()
-        .start();
-    });
+const loadJSON = (file) => {
+  return fetch(file)
+  .then(res => res.json())
+  .catch(err => console.log(err));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
