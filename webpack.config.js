7747cbe187f152cfb77e9f58e9317e154943da2d
fieldr <fieldr92@gmail.com>
2018-10-02 16:24:23 +0100

    ✨ Add plugins to webpack - 'yarn build' now compiles all data needed for running of the app - 'yarn serve' allows serving of app without any build files

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -8,7 +10,7 @@ module.exports = {
   },
   mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, 'build'),
+    contentBase: path.join(__dirname, 'src'),
     port: 3000,
     historyApiFallback: true,
     compress: true
@@ -22,7 +24,34 @@ module.exports = {
         query: {
           presets: ['env'],
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader'},
+          { loader: 'css-loader' }
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RalphCO',
+      template: 'src/index.html',
+      path: path.resolve(__dirname, 'build'),
+      filename: 'index.html'
+    }),
+    new CopyWebpackPlugin(
+      [
+        {
+          from: './src/data/data.json',
+          to: path.resolve(__dirname, 'build/data/data.json'),
+        },
+        {
+          from: './src/assets',
+          to: path.resolve(__dirname, 'build/assets')
+        }
+      ]
+    )
+  ]
 }
