6221256ad65993aa4ebb3e790338582a69408b14
fieldr <fieldr92@gmail.com>
2018-10-11 17:03:06 +0100

    ✨ Preloader complete and used in banner creation - Loader class deleted - index.js contains preload functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import Banner from './classes/Banner';
-import Loader from './classes/Loader';
+import createjs from 'createjs';
 
 import './style.css';
 
-async function init() {
-  const jsonPath = `./data/`;
-  const jsonFile = `data.json`;
+const queue = new createjs.LoadQueue(true);
+const jsonPath = `./data/`;
+const jsonFile = `data.json`;
+const assetPath = './assets/';
+const items = [];
 
+async function init() {
   try {
     const json = await loadJSON(`${jsonPath}${jsonFile}`);
-    new Loader(json)
-      .preload();
-    new Banner(json)
+    json.frames.forEach(frame => {
+      frame.layers.forEach(layer => {
+        if (layer.src.match(/[a-z]*?\.png/)) {
+          items.push( { "id": `${layer.src.match(/[a-z0-9_]*/i)}`, "src": assetPath + layer.src } )
+        }
+      });
+    });
+    queue.loadManifest(items);
+    queue.on('complete', () => {
+      new Banner(json, queue)
       .createElements()
       .animateFrames()
       .start();
+    });
   } catch (err) {
     console.log('BUILD', err);
   }
