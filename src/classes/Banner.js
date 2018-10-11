dc50c10cc02c4a5e2a6058be694226083361f29b
fieldr <fieldr92@gmail.com>
2018-10-11 20:54:45 +0100

    ♻️ Refactor index.js and Banner class

--- src/classes/Banner.js
+++ src/classes/Banner.js
@@ -5,83 +5,82 @@ import Animation from './Animation';
 import Sprite from './Sprite';
 
 export default class Banner {
-  constructor({ width, height, frames }, queue) {
+  constructor({ width, height, frames }, queue, layers) {
     this.width = width;
     this.height = height;
     this.frames = frames;
+    this.layers = layers;
     this.queue = queue;
+    this.frameDelay = 0;
     this.timeline = new TimelineLite({ paused: true });
   }
 
   createElements() {
-    this.frames.forEach(frame => {
-      frame.layers.forEach((layer, i) => {
-        const imgInfo = {
-          top: layer.top,
-          left: layer.left,
-          id: layer.src.match(/[a-z0-9_]*/i),
-          path: this.queue.getResult(layer.src.match(/[a-z0-9_]*/i))
-        };
-        const spriteInfo = {
-          top: layer.top,
-          left: layer.left,
-          width: layer.spriteWidth,
-          height: layer.spriteHeight,
-          id: layer.src.match(/[a-z0-9_]*/i),
-          path: this.queue.getResult(layer.src.match(/[a-z0-9_]*/i))
-        };
-        const divInfo = {
-          top: layer.top,
-          left: layer.left,
-          tag: 'div',
-          id: `background${i}`
-        };
-        switch (layer["type"]) {
-          case "image":
+    this.layers.forEach((layer, i) => {
+      const imgInfo = {
+        top: layer.top,
+        left: layer.left,
+        id: layer.src.match(/[a-z0-9_]*/i),
+        path: this.queue.getResult(layer.src.match(/[a-z0-9_]*/i))
+      };
+      const spriteInfo = {
+        top: layer.top,
+        left: layer.left,
+        width: layer.spriteWidth,
+        height: layer.spriteHeight,
+        id: layer.src.match(/[a-z0-9_]*/i),
+        path: this.queue.getResult(layer.src.match(/[a-z0-9_]*/i))
+      };
+      const divInfo = {
+        top: layer.top,
+        left: layer.left,
+        tag: 'div',
+        id: `background${i}`
+      };
+      switch (layer["type"]) {
+        case "image":
+          new DOMElement(imgInfo);
+          break;
+        case "spritesheet":
+          new DOMElement(spriteInfo);
+          break;
+        case "background":
+          if (layer.src.match(/[a-z]*?\.png/)) {
             new DOMElement(imgInfo);
-            break;
-          case "spritesheet":
-            new DOMElement(spriteInfo);
-            break;
-          case "background":
-            if (layer.src.match(/[a-z]*?\.png/)) {
-              new DOMElement(imgInfo);
-            } else if (layer.src.match(/#[a-f0-9]{6}/i)) {
-              new DOMElement(divInfo);
-              document.getElementById(divInfo.id).style.backgroundColor = layer.src;
-            } else {
-              console.log('Background not a image or color code');
-            }
-            break;
-          default:
-            console.log(`"${layer['type']}" is not a layer type.`);
-            break;
-        }
-      })
-    })
+          } else if (layer.src.match(/#[a-f0-9]{6}/i)) {
+            new DOMElement(divInfo);
+            document.getElementById(divInfo.id).style.backgroundColor = layer.src;
+          } else {
+            console.log('Background not a image or color code');
+          }
+          break;
+        default:
+          console.log(`"${layer['type']}" is not a layer type.`);
+          break;
+      }
+    });
     return this;
   }
 
   animateFrames() {
-    let frameDelay = 0;
     this.frames.forEach(frame => {
       frame.layers.forEach((layer, i) => {
         switch (layer["type"]) {
           case "image":
             layer.animations.forEach(animation => {
-              new Animation(layer, animation, frameDelay, this.timeline, `#${layer.src.match(/[a-z0-9_]*/i)}`)
+              new Animation(layer, animation, this.frameDelay, this.timeline, `#${layer.src.match(/[a-z0-9_]*/i)}`)
                 .styleChange();
             })
             break;
           case "background":
             if (layer.src.match(/[a-z]*?\.png/)) {
               layer.animations.forEach(animation => {
-                new Animation(layer, animation, frameDelay, this.timeline, `#${layer.src.match(/[a-z0-9_]*/i)}`)
+                new Animation(layer, animation, this.frameDelay, this.timeline, `#${layer.src.match(/[a-z0-9_]*/i)}`)
                   .styleChange();
               })
             } else if (layer.src.match(/#[a-f0-9]{6}/i)) {
               layer.animations.forEach(animation => {
-                new Animation(layer, animation, frameDelay, this.timeline, `#background${i}`)
+                new Animation(layer, animation, this.frameDelay, this.timeline, `#background${i}`)
                   .styleChange();
               })
             } else {
@@ -89,7 +88,7 @@ export default class Banner {
             }
             break;
           case "spritesheet":
-            new Sprite(layer, null, frameDelay, this.timeline, `#${layer.src.match(/[a-z0-9_]*/i)}`)
+            new Sprite(layer, null, this.frameDelay, this.timeline, `#${layer.src.match(/[a-z0-9_]*/i)}`)
               .playSprite(this.width, this.height);
             break;
           default:
@@ -97,7 +96,7 @@ export default class Banner {
             break;
         }
       });
-      frameDelay += frame["duration"];
+      this.frameDelay += frame["duration"];
     });
     return this;
   }
--- src/index.js
+++ src/index.js
@@ -6,26 +6,17 @@ import './style.css';
 const queue = new createjs.LoadQueue(true);
 const jsonPath = `./data/`;
 const jsonFile = `data.json`;
+let json;
 const assetPath = './assets/';
 const items = [];
+const layers = [];
 
 async function init() {
   try {
-    const json = await loadJSON(`${jsonPath}${jsonFile}`);
-    json.frames.forEach(frame => {
-      frame.layers.forEach(layer => {
-        if (layer.src.match(/[a-z]*?\.png/)) {
-          items.push( { "id": `${layer.src.match(/[a-z0-9_]*/i)}`, "src": assetPath + layer.src } )
-        }
-      });
-    });
-    queue.loadManifest(items);
-    queue.on('complete', () => {
-      new Banner(json, queue)
-      .createElements()
-      .animateFrames()
-      .start();
-    });
+    json = await loadJSON(`${jsonPath}${jsonFile}`);
+    createItems(json);
+    createLayers(json);
+    loader(json);
   } catch (err) {
     console.log('BUILD', err);
   }
@@ -37,6 +28,34 @@ const loadJSON = file => {
     .catch(err => {
       throw new Error(err)
     });
+}
+
+const loader = (json) => {
+  queue.loadManifest(items);
+  queue.on('complete', () => {
+    new Banner(json, queue, layers)
+    .createElements()
+    .animateFrames()
+    .start();
+  });
+}
+
+const createItems = (json) => {
+  json.frames.forEach(frame => {
+    frame.layers.forEach(layer => {
+      if (layer.src.match(/[a-z]*?\.png/)) {
+        items.push( { "id": `${layer.src.match(/[a-z0-9_]*/i)}`, "src": assetPath + layer.src } )
+      }
+    });
+  });
+}
+
+const createLayers = (json) => {
+  json.frames.forEach(frame => {
+    frame.layers.forEach(layer => {
+      layers.push(layer);
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
