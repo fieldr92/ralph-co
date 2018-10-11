109b876f1cfe826427770c35614be11302b29570
fieldr <fieldr92@gmail.com>
2018-10-11 17:04:40 +0100

    ♻️ Refactor Banner and DOMElement classes - adjustments for preloading from preloadjs

--- src/classes/DOMElement.js
+++ src/classes/DOMElement.js
@@ -1,9 +1,16 @@
 export default class DOMElement {
-  constructor({ id, path, tag = 'img', width = '100%', height = '100%', top = 0, left = 0 }) {
-    this.element = document.createElement(tag);
-    this.element.setAttribute('id', id);
-    this.element.setAttribute('style', `width: ${width}; height: ${height}; top: ${top}px; left: ${left}px; opacity: 0`);
-    if (path) this.element.setAttribute('src', path);
-    document.getElementById('stage').append(this.element);
+  constructor({ id, path, tag, width = '100%', height = '100%', top = 0, left = 0 }) {
+    if ( tag === 'div' ) {
+      this.element = document.createElement(tag);
+      this.element.setAttribute('id', id);
+      this.element.setAttribute('style', `width: ${width}; height: ${height}; top: ${top}px; left: ${left}px; opacity: 0`);
+      document.getElementById('stage').append(this.element);
+    }
+    if (!tag) {
+      this.element = path;
+      this.element.setAttribute('id', id);
+      this.element.setAttribute('style', `width: ${width}; height: ${height}; top: ${top}px; left: ${left}px; opacity: 0`);
+      document.getElementById('stage').append(this.element);
+    }
   }
 }
