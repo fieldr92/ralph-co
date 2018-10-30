3faacc7f2e6ed8993833c20fecd59989cd5dceee
fieldr <fieldr92@gmail.com>
2018-10-30 12:52:41 +0000

    🐛 Spritesheet fade in and fade out animation fix

--- src/classes/Sprite.js
+++ src/classes/Sprite.js
@@ -15,19 +15,29 @@ export default class Sprite extends Animation {
     const spriteOutDelay = `${this.frameDelay + this.layer["animation-out"]["delay"]}`;
     const spriteOutDuration = this.layer["animation-out"]["duration"];
 
+    console.log(this.layer["animation-in"]["style"]);
+
     const spriteTimeline = new TimelineMax();
+    
+    this.timeline
+      .to(this.elementId, spriteInDuration, this.layer["animation-in"]["style"], spriteInDelay);
 
     for (let i = 1; i <= noRows; i++) {
-      const topPosition = (-height * i) + this.layer.top;
+      const topPosition = (-height * i);
       const spriteSpeed = (this.layer["spriteWidth"] / width) / this.layer["framerate"];
 
-      spriteTimeline
-        .to(this.elementId, spriteInDuration, this.layer["animation-in"]["style"], spriteInDelay)
-        .to(this.elementId, spriteSpeed, { x: width - this.layer["spriteWidth"], ease:SteppedEase.config((this.layer["spriteWidth"] / width) - 1) })
-        .set(this.elementId, { top: topPosition, x: 0 });      
+      if (i === 1) {
+        spriteTimeline
+          .to(this.elementId, spriteSpeed, { x: width - this.layer["spriteWidth"], ease:SteppedEase.config((this.layer["spriteWidth"] / width) - 1) }, spriteInDelay)
+          .set(this.elementId, { top: topPosition, x: 0 }); 
+      } else {
+        spriteTimeline
+          .to(this.elementId, spriteSpeed, { x: width - this.layer["spriteWidth"], ease:SteppedEase.config((this.layer["spriteWidth"] / width) - 1) })
+          .set(this.elementId, { top: topPosition, x: 0 });
+      }
     }
 
-    spriteTimeline
+    this.timeline
       .to(this.elementId, spriteOutDuration, this.layer["animation-out"]["style"], spriteOutDelay);
   }
 }
